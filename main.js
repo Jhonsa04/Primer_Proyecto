a56cd1054167b6d1a4e5b21e3fabadbd406f3e47
unknown <SENA@VALAPRCBCFSP209.FormacionSena.RED>
2024-06-29 18:21:08 -0500

    four past

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("JHONSA04")
+alert("BIENVENIDOS A LA PAGINA DE LOS HACKERS PRROS :v")
+
+function bienvenidoUsuario(){
+    var nombre = prompt("Ingrese su nombre: ");
+    alert("Hola " + nombre + " Bienvenido prro")
+}
